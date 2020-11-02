40c385621d8f801af1cbadc0c31b72a4a6f3bf5c
pseudoyu <pseudoyu@connect.hku.hk>
2020-11-02 23:44:42 +0800

    Problem 1000 Done

---
+++ problem1000.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class problem1000 {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int a = in.nextInt();
+	  int b = in.nextInt();
+	  in.close();
+	  System.out.println(a + b);
+   }
+}
