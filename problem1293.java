0978f7470a06dc36dbc0751ac536c4d044b2a49e
pseudoyu <pseudoyu@connect.hku.hk>
2020-11-03 00:40:53 +0800

    Problem 1293 Done

---
+++ problem1293.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class problem1293 {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+	  int a = in.nextInt();
+	  int b = in.nextInt();
+	  in.close();
+	  System.out.println(n * a * b * 2);
+   }
+}
