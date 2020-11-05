0bc1cb9811fb40e25654b77c042babdcfe34d76e
pseudoyu <pseudoyu@connect.hku.hk>
2020-11-06 01:31:54 +0800

    Problem 1264 Done

---
+++ problem1264.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class problem1264 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		in.close();
+
+		System.out.println((n*(m+1)));
+	}
+}
