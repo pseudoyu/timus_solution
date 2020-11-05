4c782d4e39e4f5d787bf2af316fa17b6a41b6c5d
pseudoyu <pseudoyu@connect.hku.hk>
2020-11-06 01:07:33 +0800

    Problem 2001 Done

---
+++ problem2001.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class problem2001 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a1 = in.nextInt();
+		int b1 = in.nextInt();
+		int a2 = in.nextInt();
+		int b2 = in.nextInt();
+		int a3 = in.nextInt();
+		int b3 = in.nextInt();
+		in.close();
+
+		int m1 = a1 - a3;
+		int m2 = b1 - b2;
+
+		System.out.println(m1 + " " + m2);
+   }
+}
